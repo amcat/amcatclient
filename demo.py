b88fe1a028b24a841580ad04b1ef53520ef5e3a2
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-04-19 02:54:25 +0200

    updated demo

--- demo.py
+++ demo.py
@@ -1,11 +1,17 @@
+import argparse
 from amcatclient import AmcatAPI
-import sys
 
-host, username, password = sys.argv[1:]
+# Connect to AmCAT
+parser = argparse.ArgumentParser()
+parser.add_argument('host', help='The AmCAT host to connect to, '
+                    'e.g. http://amcat.vu.nl')
+parser.add_argument('--username', help='Username for AmCAT login')
+parser.add_argument('--password', help='Password for AmCAT login')
+args = parser.parse_args()
 
-api = AmcatAPI(host, username, password)
+conn = AmcatAPI(args.host, args.username, args.password)
 
-articles_json = [
+articles = [
     {"headline" : "test headline3",
      "medium" : "test medium",
      "text" : "test text",
@@ -19,5 +25,7 @@ articles_json = [
     ]
 
 aset = api.create_set(project=1, name="Testset", provenance="test data")
-articles = api.create_articles(project=1, articleset=aset["id"], json_data=articles_json)
-print "Added {n} articles to set {setid}".format(n=len(articles), setid=aset["id"])
+articles = api.create_articles(project=1, articleset=aset["id"],
+                               json_data=articles)
+print "Added {n} articles to set {setid}".format(n=len(articles),
+                                                 setid=aset["id"])
